e478935598aacc9081b96760371389c8b550c2aa
Ankush Kumar <ankushkumar9618@gmail.com>
2025-03-29 01:44:46 +0530

    Updated Customer Model

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 
 const customerSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    }
 })
 
 export const Customer = mongoose.model("Customer", customerSchema);
