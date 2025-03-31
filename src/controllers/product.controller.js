c81b84191ed0a2ab99bb759b54a332982a9d07e8
Ankush Kumar <ankushkumar9618@gmail.com>
2025-04-01 02:12:32 +0530

    Added controller to getAllProducts and getProduct and craeted its routes

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -49,10 +49,72 @@ const deleteProduct = asyncHandler(async (req, res) => { //Deleting New Product
 })
 
 const updateProduct = asyncHandler(async (req, res) => {//Updating Product Details
+    const { productId } = req.params;
+    let { productName, description, price, category } = req.body;
+    const product = await Product.findById(productId);
+    if (product.name == productName && product.description == description && product.price == price && product.category == category) {
+        throw new ApiError(400, "Atleast One Field Must be Modified to Update");
+    }
+
+    if (!productName && !price) {
+        throw new ApiError(400, "Name and Price is required to Update");
+    }
+    const updatedProduct = await Product.findByIdAndUpdate(
+        productId,
+        {
+            name: productName,
+            description,
+            price,
+            category
+        },
+        {
+            new: true
+        }
+    )
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "Product Updated Succesfully", updatedProduct)
+        )
+
+
+})
+
+const getAllProducts = asyncHandler(async (req, res) => {
+    const product = await Product.find();
+    if (!product) {
+        throw new ApiError(400, "Something went wrong While Getting Products");
+    }
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "Products Fetched Successfully", product)
+        )
+})
+
+const getProduct = asyncHandler(async (req, res) => {
+    const { productId } = req.params;
+    if (!productId) {
+        throw new ApiError(400, "Invalid Product Id")
+    }
+    const product = await Product.findById(productId);
+    if (!product) {
+        throw new ApiError(404, "Product Not Found");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "Product Fetched Successfully", product)
+        )
 })
 
 
 export {
     addProduct,
-    deleteProduct
+    deleteProduct,
+    updateProduct,
+    getAllProducts,
+    getProduct
 };
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { isAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
-import { addProduct, deleteProduct } from "../controllers/product.controller.js";
+import { addProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
-
-router.route("/create").post(verifyJWT, isAdmin, addProduct);
-router.route("/delete/:productId").delete(verifyJWT, isAdmin, deleteProduct)
+router.route("/").get(verifyJWT, getAllProducts);
+router.route("/:productId").get(verifyJWT, getProduct);
+router.route("/add").post(verifyJWT, isAdmin, addProduct);
+router.route("/delete/:productId").delete(verifyJWT, isAdmin, deleteProduct);
+router.route("/update/:productId").patch(verifyJWT, isAdmin, updateProduct);
 
 export default router;
