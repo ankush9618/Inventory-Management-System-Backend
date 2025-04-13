0e0973d08791133e89d7b5cec7c6b396d6caa408
Ankush Kumar Singh <70734844+ankush9618@users.noreply.github.com>
2025-04-14 02:52:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import inventoryRouter from "./routes/inventory.routes.js"
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
