7ff96b89ab3c1947d451b368473e55e1b279aa92
Ankush Kumar <ankushkumar9618@gmail.com>
2025-04-16 01:49:56 +0530

    Modified constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
 export const DB_NAME = "IMS-DB"
 
-export const COOKIE_OPTIONS = { httpOnly: true, secure: false,sameSite: 'Lax' }
+export const COOKIE_OPTIONS = {
+    httpOnly: true,
+    secure: true, // Ensure HTTPS is used
+    sameSite: "none", // Required for cross-origin cookies
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+};
